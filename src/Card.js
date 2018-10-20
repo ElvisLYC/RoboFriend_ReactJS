8148242ca5d4be6151718709bcc7a7acfb94c24f
Elvis <yinchuang94@gmail.com>
2018-10-20 16:14:30 +0800

    added search filter

--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className='tc bg-light-green dib br3 ma2 grow'>
+    <div className='tc bg-light-green dib br3 ma2 w-20-m grow'>
       <img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2>{name}</h2>
